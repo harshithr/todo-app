a8a07e9ded6d2b1c336874b5b11f32bfac667d88
Harshith <harshithr.pro@gmail.com>
2017-02-01 09:58:19 +0530

    addded todos/:id
    check if id is valid or not

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var Todo = require("./models/todo");
 var User = require("./models/user");
 var express = require("express");
 const bodyParser = require("body-parser");
+var {ObjectID} = require("mongodb");
 
 const app = express();
 
@@ -17,6 +18,33 @@ app.post("/todos", function(req, res){
     }, function(err){
         res.status(400).send(err);
     });
+});
+
+app.get("/todos", function(req, res){
+    Todo.find().then(function(data){
+        res.send(data);
+    }, function(err){
+        res.status(400).send(err);
+    });
+});
+
+app.get("/todos/:id", function(req, res){
+   var id = req.params.id;
+
+   if(!ObjectID.isValid(id)){
+       return res.status(404).send();
+   }
+
+   Todo.findById(id).then(function(data){
+       if(!data){
+            return res.status(404).send();
+       }
+
+       res.send(data);
+
+   }).catch(function(err){
+        res.status(400).send();
+   });
 });
 
 app.listen(3000, function(){
